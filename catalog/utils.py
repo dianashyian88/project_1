656231a9215417f7e89d217c3187b4add87f7247
dianashyian88 <dvshyian@gmail.com>
2023-11-11 21:59:19 +0300

    update hw_23_2 (get_categories_cache)

--- catalog/utils.py
+++ catalog/utils.py
@@ -6,10 +6,11 @@ def get_categories_cache():
     key = 'category_list'
 
     if CACHE_ENABLED:
-       return cache.get(key)
-
-    category_list = Category.objects.all()
-    if CACHE_ENABLED:
-        cache.set(key, category_list)
+        category_list = cache.get(key)
+        if category_list is None:
+            category_list = Category.objects.all()
+            cache.set(key, category_list)
+    else:
+        category_list = Category.objects.all()
 
     return category_list
